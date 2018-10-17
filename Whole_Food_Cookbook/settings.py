746749bafb2219ac07abdc7a247c43ef05672dde
darmonlyonelab <manusporn.f@ku.th>
2018-10-17 10:46:45 +0700

    re change setting

--- Whole_Food_Cookbook/settings.py
+++ Whole_Food_Cookbook/settings.py
@@ -112,11 +112,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
-#
 
-STATICFILES_DIR = [
-    "/start/static/"
-]
+# STATICFILES_DIR = [
+#     os.path.join(BASE_DIR, 'static'),
+#     "WholeFoodBook/start/static"
+# ]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
