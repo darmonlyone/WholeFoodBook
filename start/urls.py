93ee010565e1f9f1ccd717eec57406b1f6cf4ccf
darmonlyonelab <manusporn.f@ku.th>
2018-10-17 08:32:41 +0700

    update django welcome template

--- start/urls.py
+++ start/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'start'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('welcome/', views.welcome, name='welcome'),
 ]
--- start/views.py
+++ start/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template.loader import render_to_string
 
 
@@ -6,3 +7,6 @@ def index(request):
     rendered = render_to_string('index.html')
     return HttpResponse(rendered)
 
+
+def welcome(request):
+    return render(request, 'welcome.html')
