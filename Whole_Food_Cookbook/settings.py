92c97f30e0dbe2375fc7bd0edc362abf639333d0
darmonlyonelab <manusporn.f@ku.th>
2018-10-17 21:24:55 +0700

    try deploy

--- Whole_Food_Cookbook/settings.py
+++ Whole_Food_Cookbook/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,4 +119,6 @@ STATICFILES_DIR = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
