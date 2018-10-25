7ff9939a11fdd0d91dbf791098e661f2d742b97c
darmonlyonelab <manusporn.f@ku.th>
2018-10-25 20:42:39 +0700

    setting make media roll and urls

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = 'cookbook'
@@ -8,3 +9,5 @@ urlpatterns = [
     path('menu/', views.MenuView.as_view(), name='menu'),
     # path('database/', views.fake_put_db),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
