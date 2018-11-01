153f345d282eca6c753db9d88b255f454d93e7f0
Pichaaun Popukdee <pichaaun.p@ku.th>
2018-11-01 11:30:00 +0700

    fixed display webpage

--- cookbook/templates/displayController.js
+++ cookbook/templates/displayController.js
@@ -1,66 +1,104 @@
-$('#controlR').click(function() {
-    event.preventDefault();
-    $('#content').animate({
-      marginLeft: "-=400px"
-    }, "fast");
- });
+var size = 760;
+var margin = 20;
+var count = 6;
+var visible = 3; // Visible carousel slides (excluding the barely)
+var last = count - visible; // 3
+var offset = 0;
+var carousel = (size * visible) + (margin * visible) + (size / 3);
+var container = (size * count) + (margin * count);
+var barely = size / visible;
 
-$('#controlL').click(function() {
-    event.preventDefault();
-    $('#content').animate({
-      marginLeft: "+=400px"
-    }, "fast");
+var $container = $('.carousel__inner');
+var $slides = $('.carousel__box');
+var $left = $('.carousel__control--left');
+var $right = $('.carousel__control--right');
+var $previous = null;
+var $next = null;
+
+var enter = null;
+var close = null;
+
+$left.on('click', function() {
+  if ( offset === 0 ) return;
+  move(--offset);
 });
 
+$right.on('click', function() {
+  if ( offset === count - visible ) return;
+  move(++offset);
+});
 
+$slides.each(function(index, slide) {
+  $(slide).data('index', index);
+});
 
-/*jQuery(document).ready(function ($) {
-
-
-
-  var slideCount = $('.module-section ul li').length;
-  var slideWidth = $('.module-section ul li').width();
-  var slideHeight = $('.module-section ul li').height();
-  var sliderUlWidth = slideCount * slideWidth;
+$slides.on('mouseenter', _.debounce(function() {
+  var $slide = $(this);
+  var index = $slide.data('index');
+  $previous = previous(index);
+  $next = next(index);
   
-  $('.module-section').css({ width: slideWidth, height: slideHeight });
+  $previous.addClass('carousel__box--previous');
+  $next.addClass('carousel__box--next');
+  $slide.addClass('carousel__box--enter')
+}, 350));
+
+$slides.on('mouseout', _.debounce(function() {
+  var $slide = $(this);
   
-  $('.module-section ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+  $slide
+    .addClass('carousel__box--leave')
+    .removeClass('carousel__box--enter')
+    .delay(400)
+    .queue(function() {
+      $(this).removeClass('carousel__box--leave')
+        .dequeue();
+    });
   
-  $('.module-section ul li:last-child').prependTo('.module-section ul');
+  $previous.addClass('carousel__box--previous-leave')
+    .removeClass('carousel__box--previous')
+    .delay(300)
+    .queue(function() {
+      $(this).removeClass('carousel__box--previous-leave')
+        .dequeue();
+    });
+  
+  $next.addClass('carousel__box--next-leave')
+    .removeClass('carousel__box--next')
+    .delay(300)
+    .queue(function() {
+      $(this).removeClass('carousel__box--next-leave')
+        .dequeue();
+    });
+}, 300));
 
-  function moveLeft() {
-      $('.module-section ul').animate({
-          left: + slideWidth
-      }, 200, function () {
-          $('.module-section ul li:last-child').prependTo('.module-section ul');
-          $('.module-section ul').css('left', '');
-      });
-  };
+function previous(hovered) {
+  // Index of the hovered slide in the current offset
+  var index = hovered - offset;
+  
+  // We could have this as start = offset, but we have
+  // a weird slider presented here haha.
+  var start = offset + visible === count
+    ? offset - 1
+    : offset;
+  
+  return $slides.slice(start, offset + index);
+}
 
-  function moveRight() {
-      $('.module-section ul').animate({
-          left: - slideWidth
-      }, 200, function () {
-          $('.module-section ul li:first-child').appendTo('.module-section ul');
-          $('.module-section ul').css('left', '');
-      });
-  };
+function next(hovered) {
+  // Index of the hovered slide in the current offset
+  var index = hovered - offset;
+  
+  if ( index === visible ) {
+    return $slides.slice();
+  } else {
+    return $slides.slice(offset + index + 1, offset + visible + 1);
+  }
+}
 
-  $('.left-controls').click(function () {
-      moveLeft();
-  });
-
-  $('.right-controls').click(function () {
-      moveRight();
-  });
-
-});    
-
-
-
-*/
-
-
-
-
+function move(offset) {
+  var translateX = offset === last
+    ? -(container - carousel - margin)
+    : -((size * offset) + (margin * offset));
+  $container.css('transform', 'translateX(' + translateX + 'px)'); 
+}
