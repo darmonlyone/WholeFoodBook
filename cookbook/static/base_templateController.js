d852a2075eb8dee6ff1aa9d514aad184eb32adc3
Raksani <raksani.k@ku.th>
2018-11-07 14:34:10 +0700

    Add link to profile popup, after click the "Login", modal close and "Login" changes to "Manusporn"

---
+++ cookbook/static/base_templateController.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('#fb-login').click(function () {
+        console.log("click");
+        $("#start-login").text('Manusporn');
+    });
+
+    $('#gg-login').click(function () {
+        console.log("click");
+        $("#start-login").text('Manusporn');
+    });
+ });
--- cookbook/urls.py
+++ cookbook/urls.py
@@ -5,9 +5,8 @@ from . import views
 
 app_name = 'cookbook'
 urlpatterns = [
-    path('',views.ProfileViewTest.as_view(), name='popup'),
-    # path('', views.WelcomeView.as_view(), name='welcome'),
-    # path('recipe/<str:recipe_name>', views.MenuView.as_view(), name='recipe'),
-    path ('profile/',views.ProfileView.as_view(), name='profile'),
-    # # path('database/', views.fake_put_db),
+    path('', views.WelcomeView.as_view(), name='welcome'),
+    path('recipe/<str:recipe_name>', views.MenuView.as_view(), name='recipe'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    # path('database/', views.fake_put_db),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
