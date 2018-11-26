55e593fd7fd156a41683b545f73229c444640368
Pichaaun Popukdee <pichaaun.p@ku.th>
2018-11-26 19:03:09 +0700

    edit clearfilter and filter feature

--- cookbook/static/script/categoriesController.js
+++ cookbook/static/script/categoriesController.js
@@ -1,9 +1,16 @@
 var btnContainer = document.getElementById("category-button");
 var btns = btnContainer.getElementsByClassName("btncat");
+var filterbut = $('.check:checked');
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(){
     var current = document.getElementsByClassName("actived");
     current[0].className = current[0].className.replace(" actived", "");
     this.className += " actived";
+    console.log('yeahhh!!');
+    if (filterbut.length > 0 ) {
+      for (var j =0 ; j < filterbut.length ; j ++ ) {
+        filterbut[j].checked = false;
+      }
+    } 
   });
 }
