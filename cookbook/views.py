b8c2340af55181ba304a9fac26f033c45b4b832f
Raksani <raksani.k@ku.th>
2018-11-28 17:40:55 +0700

    add search fx

--- cookbook/views.py
+++ cookbook/views.py
@@ -1,24 +1,13 @@
+from django.shortcuts import render
 from random import randint
-
-from django.contrib.auth import logout as auth_logout
-from django.http import HttpResponseRedirect, Http404
-from django.urls import reverse
 from django.views import generic
-
-from cookbook.forms import UserAliasForm
-from cookbook.models import Recipe, AuthorUser, UserAlias
-
+from cookbook.models import Recipe
 
 class ProfileView(generic.ListView):
     template_name = 'profile.html'
-    model = Recipe
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['author_user'] = AuthorUser.objects.all()
-        context['recipe_all'] = Recipe.objects.all()
-        context['author_alias'] = UserAlias.objects.all()
-        return context
+    def get_queryset(self):
+        return None
 
 
 class WelcomeView(generic.ListView):
@@ -30,20 +19,12 @@ class WelcomeView(generic.ListView):
 
 class MenuView(generic.ListView):
     template_name = 'menu.html'
+    context_object_name = 'recipe_enable'
     model = Recipe
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get_queryset(self):
         recipe_name = self.kwargs['recipe_name']
-        recipe_all = Recipe.objects.all()
-        if recipe_all.count() > 8:
-            random_int = randint(0, recipe_all.all().count() - 9)
-            context['recipe_suggest'] = recipe_all[random_int:random_int + 8]
-        else:
-            context['recipe_suggest'] = recipe_all
-        context['recipe_enable'] = recipe_all.filter(recipe_name=recipe_name)
-        context['author_user'] = AuthorUser.objects.all()
-        return context
+        return Recipe.objects.filter(recipe_name=recipe_name)
 
 
 class LoginView(generic.ListView):
@@ -55,56 +36,16 @@ class LoginView(generic.ListView):
 
 class IndexView(generic.ListView):
     template_name = 'display.html'
+    context_object_name = 'recipe_random'
     model = Recipe
 
-    def dispatch(self, request, *args, **kwargs):
-        return super(IndexView, self).dispatch(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get_queryset(self):
         recipe_all = Recipe.objects.all()
         if recipe_all.count() > 8:
             random_int = randint(0, recipe_all.all().count() - 9)
-            context['recipe_suggest'] = recipe_all[random_int:random_int + 8]
+            return Recipe.objects.all()[random_int:random_int + 8]
         else:
-            context['recipe_suggest'] = recipe_all
-        context['recipe_all'] = recipe_all
-        context['recipe_appetizer'] = recipe_all.filter(category_tags__food_category='appetizer')
-        context['recipe_main'] = recipe_all.filter(category_tags__food_category='main dish')
-        context['recipe_soup'] = recipe_all.filter(category_tags__food_category='soup and stew')
-        context['recipe_dessert'] = recipe_all.filter(category_tags__food_category='dessert')
-        context['recipe_drink'] = recipe_all.filter(category_tags__food_category='drink')
-        context['recipe_salad'] = recipe_all.filter(category_tags__food_category='salad')
-        return context
-
-
-def logout(request):
-    """Logs out user"""
-    auth_logout(request)
-    return HttpResponseRedirect(reverse("cookbook:index"))
-
-
-def userAliasPost(request):
-    if request.method == 'POST':
-        form = UserAliasForm(request.POST)
-        if not form.is_valid():
-            user_name = request.POST.get('user_name', '')
-            alias = request.POST.get('alias_name', '')
-            alias_model = UserAlias(user_username=user_name, alias_name=alias)
-            alias_model.save()
-            return HttpResponseRedirect(reverse("cookbook:profile"))
-        raise Http404
-
-
-class DeleteRecipeView(generic.ListView):
-    def post(self, request, *args, **kwargs):
-        delete_recipe = request.POST.get('deleted_recipe', '')
-        user_name = request.POST.get('user_name', '')
-        delete_recipe_model = Recipe.objects.get(recipe_name__exact=delete_recipe)
-        delete_recipe_model.delete()
-        author_recipe = AuthorUser.objects.get(recipe_name__exact=delete_recipe, user_username__exact=user_name)
-        author_recipe.delete()
-        return HttpResponseRedirect(reverse("cookbook:profile"))
+            return recipe_all
 
 
 # def test(request):
@@ -114,17 +55,14 @@ class DeleteRecipeView(generic.ListView):
 
 # def fake_put_db(request):
 #
-# r = Recipe(recipe_chef="Wan", recipe_name="Chicken Breast", recipe_info="Pumpkin Pie? Pudding shots? Obviously the
-# two belong together! Fireball adds some more cinnamon flavor to the shots but you can sub with all vodka if you
-# prefer. ", recipe_time=80, recipe_type="Grill", recipe_fat=450, recipe_ingredient="2 x 400g:packets Lilydale Breast
-#  Strips||8:kipfler potatoes, washed, cut into 1cm thick rounds||1:tablespoon olive oil||100g:cup macadamia
-# nuts||1/2:garlic clove, chopped||1:tablespoon water||1:baby beans, ends trimmed, blanched", recipe_method="Preheat
-# oven to 200°C or 180°C fan. Line two baking trays with baking paper. Lay Lilydale breast strips on one of the
-# prepared trays and bake according to packet instructions||Place potatoes onto the other prepared tray in a single
-# layer. Drizzle with oil and season well. Bake for 25-30 minutes, until golden and tender||Meanwhile, make pesto.
-# Place watercress, macadamias and garlic in a small food processor. Pulse until chopped. Add extra virgin olive oil,
-#  lemon juice and water. Process until a paste forms. Transfer hot potatoes to a bowl, add half of the pesto and
-# stir to coat") r.save()
+# r = Recipe(recipe_chef="Wan", recipe_name="Chicken Breast",
+#            recipe_info="Pumpkin Pie? Pudding shots? Obviously the two belong together! Fireball adds some more cinnamon flavor to the shots but you can sub with all vodka if you prefer. ",
+#            recipe_time=80,
+#            recipe_type="Grill",
+#            recipe_fat=450,
+#            recipe_ingredient="2 x 400g:packets Lilydale Breast Strips||8:kipfler potatoes, washed, cut into 1cm thick rounds||1:tablespoon olive oil||100g:cup macadamia nuts||1/2:garlic clove, chopped||1:tablespoon water||1:baby beans, ends trimmed, blanched",
+#            recipe_method="Preheat oven to 200°C or 180°C fan. Line two baking trays with baking paper. Lay Lilydale breast strips on one of the prepared trays and bake according to packet instructions||Place potatoes onto the other prepared tray in a single layer. Drizzle with oil and season well. Bake for 25-30 minutes, until golden and tender||Meanwhile, make pesto. Place watercress, macadamias and garlic in a small food processor. Pulse until chopped. Add extra virgin olive oil, lemon juice and water. Process until a paste forms. Transfer hot potatoes to a bowl, add half of the pesto and stir to coat")
+# r.save()
 
 # heroku pg:psql
 # \d
